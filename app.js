bf1b2931ab0e4a19f198491288d5b42ca33693d1
Alexis Velez <aldavequfalcon9999@gmail.com>
2026-08-18 16:50:08 -0500

    Actualizacion nueva desde main en primera cuenta

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("practica de git and git hub")
 let a = 10;
-const est = [{nombre:1},{nombre:2}]
+const est = [{ nombre: 1 }, { nombre: 2 }]
+const map = est.map(({ nombre, id }) => ({ id, nombre }))
+console.log(map);
+
