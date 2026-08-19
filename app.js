ddd530c9e8306aafdb4d6eaa54e3f54ed2e21aeb
adqvelez-cloud <adq.velez@yavirac.edu.ec>
2026-08-19 07:00:02 -0500

    rebase con main desde cuenta 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("practica de git and git hub")
-let a = 10;
+
 const est = [{ nombre: 1 }, { nombre: 2 }]
 const map = est.map(({ nombre, id }) => ({ id, nombre }))
 console.log(map);
@@ -11,3 +11,9 @@ const obj = {
 
 
 }
+class animal {
+  constructor(parameters) {
+
+  }
+}
+
